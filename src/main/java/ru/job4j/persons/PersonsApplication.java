6284ab26d559c4c9105aeadd2b5e22c247ad9844
Artem Averin <artem-averin-91@mail.ru>
2024-03-30 18:06:28 +0300

    3. Авторизация JWT [#9146]

--- src/main/java/ru/job4j/persons/PersonsApplication.java
+++ src/main/java/ru/job4j/persons/PersonsApplication.java
@@ -2,11 +2,18 @@ package ru.job4j.persons;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class PersonsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonsApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
--- src/main/java/ru/job4j/persons/repository/PersonRepository.java
+++ src/main/java/ru/job4j/persons/repository/PersonRepository.java
@@ -1,10 +1,13 @@
 package ru.job4j.persons.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.persons.model.Person;
 
 import java.util.List;
 public interface PersonRepository extends CrudRepository<Person, Integer> {
     List<Person> findAll();
+
+    Person findByLogin(String username);
 
 }

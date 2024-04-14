b9ae82a4bc04626c9f422193d659b586c53a9a2b
Artem Averin <artem-averin-91@mail.ru>
2024-04-14 21:46:26 +0300

    5. Обработка исключений и Spring REST [#504797]

--- src/main/java/ru/job4j/persons/controller/PersonController.java
+++ src/main/java/ru/job4j/persons/controller/PersonController.java
@@ -88,4 +88,5 @@ public class PersonController {
         }}));
         LOGGER.error(e.getLocalizedMessage());
     }
+
 }
--- src/main/java/ru/job4j/persons/handler/GlobalExceptionHandler.java
+++ src/main/java/ru/job4j/persons/handler/GlobalExceptionHandler.java
@@ -33,4 +33,5 @@ public class GlobalExceptionHandler {
         }}));
         LOGGER.error(e.getMessage());
     }
+
 }
